7443c67d4cae7f6553eacce692e3f4a8dfacf906
Christian Garcia <kriz10@outlook.com>
2023-09-09 13:36:27 -0600

    adding whitelist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    base_url: 'https://boxmakercel-eaa68f8e9648.herokuapp.com/'
+    base_url: 'https://boxmakercel-eaa68f8e9648.herokuapp.com'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    base_url: 'https://boxmakercel-eaa68f8e9648.herokuapp.com/'
+    base_url: 'https://boxmakercel-eaa68f8e9648.herokuapp.com'
 }
